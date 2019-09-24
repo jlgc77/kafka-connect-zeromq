795aaceb7bfea8e30242239021329f0539c073b8
Jose <jlgc77@gmail.com>
2019-09-24 16:22:32 +0200

    ejemplo que lee y pasa a una clase java

---
+++ src/main/java/io/wizzie/kafka/connect/zeromq/SampleAleFeedReader.java
@@ -0,0 +1,55 @@
+package io.wizzie.kafka.connect.zeromq;
+
+import java.io.IOException;
+
+import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
+
+import com.google.common.primitives.Bytes;
+
+import io.wizzie.kafka.connect.zeromq.model.Schema.nb_event;
+import io.wizzie.kafka.connect.zeromq.model.Schema.proximity;
+
+public class SampleAleFeedReader {
+
+	public static void main(String[] args) throws IOException {
+		Context context = ZMQ.context(1);
+
+		Socket socket = context.socket(ZMQ.SUB);
+		socket.connect("tcp://192.168.223.43:7779");
+		// subscribe to all available topics
+//		socket.subscribe("".getBytes());
+		socket.subscribe("proximity".getBytes());
+//		socket.subscribe("presence".getBytes());
+
+		byte[] currByte = null;
+		String currentAddress = null;
+
+		while (!Thread.currentThread().isInterrupted()) {
+			currentAddress = socket.recvStr(0);
+			currByte = socket.recv(0);
+
+			while (socket.hasReceiveMore()) {
+				System.out.println("multi-part zmq message ");
+				byte[] moreBytes = socket.recv(0);
+				currByte = Bytes.concat(currByte, moreBytes);
+			}
+
+			nb_event evento = nb_event.parseFrom(currByte);
+			System.out.println("EVENTO >>> ");
+			System.out.println(evento);
+
+			/**
+			 * Datos proximity
+			 */
+			proximity item = proximity.parseFrom(currByte);
+			System.out.println("Entidad >>> ");
+//			System.out.println(item);
+			System.out.println("Mac address client >>> ");
+			System.out.println(item.getStaEthMac());
+			System.out.println(item.getHashedStaEthMac());
+			System.out.println("Fin mac >>> ");
+		}
+	}
+}
