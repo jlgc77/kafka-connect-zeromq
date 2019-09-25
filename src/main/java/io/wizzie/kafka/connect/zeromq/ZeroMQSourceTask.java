2ce658ae40821d728856bd067443397e2918b427
Jose <jlgc77@gmail.com>
2019-09-25 09:23:50 +0200

    manda mensajes de zeromq a kafka

--- src/main/java/io/wizzie/kafka/connect/zeromq/ZeroMQSourceTask.java
+++ src/main/java/io/wizzie/kafka/connect/zeromq/ZeroMQSourceTask.java
@@ -11,45 +11,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
 
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import io.wizzie.kafka.connect.zeromq.model.Schema.proximity;
+
 public class ZeroMQSourceTask extends SourceTask {
 
 	private static Logger log = LoggerFactory.getLogger(ZeroMQSourceTask.class);
-	
+
 	private String topic = null;
 	private ZMQ.Socket subscriber = null;
 	private ZMQ.Context context = null;
 	private BlockingQueue<ZeroMQMessageProcessor> wQueue = new LinkedBlockingQueue<>();
 	private boolean isRunning = true;
-	
+
 	private ZeroMQSourceConnectorConfig zeroMQSourceConnectorConfig = null;
-	
+
 	@Override
 	public String version() {
 		return new ZeroMQSourceConnector().version();
 	}
-	
+
 	private void startSubscriber() {
 		log.info("Starting subscriber");
 		new Thread(new Runnable() {
 			public void run() {
 				while (isRunning) {
-					
+
 					byte[] message = subscriber.recv();
 					log.info("Received new message!");
 					try {
-						
-						wQueue.put(
-								zeroMQSourceConnectorConfig.getConfiguredInstance("message_processor_class", ZeroMQMessageProcessor.class)
-										.process(topic, new ZeroMQMessage(topic, new String(message))));
-					} catch (InterruptedException e) {
+
+						proximity item = proximity.parseFrom(message);
+						String sta_eth_mac = byteStringToStringForMac(item.getHashedStaEthMac());
+
+						wQueue.put(zeroMQSourceConnectorConfig
+								.getConfiguredInstance("message_processor_class", ZeroMQMessageProcessor.class)
+								.process(topic, new ZeroMQMessage(topic, sta_eth_mac)));
+					} catch (InterruptedException | InvalidProtocolBufferException e) {
 						e.printStackTrace();
 					}
-					
-				}		
+
+				}
 			}
 		}).start();
-	} 
-	
+	}
+
+	public static String byteStringToStringForMac(ByteString byteStr) {
+		String result = "";
+		for (int i = 0; i < byteStr.size(); ++i) {
+			if (i != 0)
+				result += ":";
+			result += String.format("%02X", byteStr.byteAt(i));
+		}
+		return result;
+	}
 
 	@Override
 	public void start(Map<String, String> props) {
@@ -58,13 +75,16 @@ public class ZeroMQSourceTask extends SourceTask {
 		zeroMQSourceConnectorConfig = new ZeroMQSourceConnectorConfig(props);
 
 		topic = props.get(ZeroMQSourceConnector.TOPIC_CONFIG);
-		
+
 		context = ZMQ.context(1);
-		
+
 		subscriber = context.socket(ZMQ.SUB);
-		subscriber.connect("tcp://192.168.1.141:5555");
-		subscriber.subscribe("toutiao".getBytes());
-		
+//		subscriber.connect("tcp://192.168.1.141:5555");
+//		subscriber.subscribe("toutiao".getBytes());
+
+		subscriber.connect("tcp://192.168.223.43:7779");
+		subscriber.subscribe("proximity".getBytes());
+
 		startSubscriber();
 	}
 
