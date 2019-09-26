6719d564b358382c2b0fa08825270b4fa7d98568
Jose <jlgc77@gmail.com>
2019-09-26 07:49:11 +0200

    task que funciona y filtra del evento el tipo

--- src/main/java/io/wizzie/kafka/connect/zeromq/ZeroMQSourceTask.java
+++ src/main/java/io/wizzie/kafka/connect/zeromq/ZeroMQSourceTask.java
@@ -10,10 +10,16 @@ import org.apache.kafka.connect.source.SourceTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
+import org.zeromq.ZMQ.Socket;
 
+import com.google.common.primitives.Bytes;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 
+import io.wizzie.kafka.connect.zeromq.model.Schema.location;
+import io.wizzie.kafka.connect.zeromq.model.Schema.nb_event;
+import io.wizzie.kafka.connect.zeromq.model.Schema.presence;
 import io.wizzie.kafka.connect.zeromq.model.Schema.proximity;
 
 public class ZeroMQSourceTask extends SourceTask {
@@ -35,27 +41,54 @@ public class ZeroMQSourceTask extends SourceTask {
 
 	private void startSubscriber() {
 		log.info("Starting subscriber");
-		new Thread(new Runnable() {
-			public void run() {
-				while (isRunning) {
+		Context context = ZMQ.context(1);
 
-					byte[] message = subscriber.recv();
-					log.info("Received new message!");
-					try {
+		Socket socket = context.socket(ZMQ.SUB);
+		socket.connect("tcp://192.168.223.43:7779");
+		// subscribe to all available topics
+		socket.subscribe("".getBytes());
+//		socket.subscribe("proximity".getBytes());
+//		socket.subscribe("presence".getBytes());
 
-						proximity item = proximity.parseFrom(message);
-						String sta_eth_mac = byteStringToStringForMac(item.getHashedStaEthMac());
+		byte[] currByte = null;
+		String currentAddress = null;
+		nb_event evento = null;
 
-						wQueue.put(zeroMQSourceConnectorConfig
-								.getConfiguredInstance("message_processor_class", ZeroMQMessageProcessor.class)
-								.process(topic, new ZeroMQMessage(topic, sta_eth_mac)));
-					} catch (InterruptedException | InvalidProtocolBufferException e) {
-						e.printStackTrace();
-					}
+		while (!Thread.currentThread().isInterrupted()) {
+			currentAddress = socket.recvStr(0);
+			currByte = socket.recv(0);
+			// System.out.println("CurrentAdress es >>> " + currentAddress);
 
-				}
+			while (socket.hasReceiveMore()) {
+				System.out.println("Entro en while del socker");
+				byte[] moreBytes = socket.recv(0);
+				currByte = Bytes.concat(currByte, moreBytes);
 			}
-		}).start();
+
+			try {
+				evento = nb_event.parseFrom(currByte);
+			} catch (InvalidProtocolBufferException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (evento.getPresence() != null) {
+				System.out.println("es presence");
+				presence presence = evento.getPresence();
+				System.out.println("es presence to strig  >> " + presence.toString());
+			} else if (evento.getProximity() != null) {
+				System.out.println("es proximity");
+				proximity proximity = evento.getProximity();
+				System.out.println("es proximity to strig  >> " + proximity.toString());
+			} else if (evento.getLocation() != null) {
+				System.out.println("es location");
+				location location = evento.getLocation();
+				System.out.println("latitude >> " + location.getLatitude());
+				System.out.println("longitude >>" + location.getLongitude());
+			} else {
+				System.out.println("es otro");
+			}
+		}
+
 	}
 
 	public static String byteStringToStringForMac(ByteString byteStr) {
@@ -76,14 +109,14 @@ public class ZeroMQSourceTask extends SourceTask {
 
 		topic = props.get(ZeroMQSourceConnector.TOPIC_CONFIG);
 
-		context = ZMQ.context(1);
+//		context = ZMQ.context(1);
 
-		subscriber = context.socket(ZMQ.SUB);
+//		subscriber = context.socket(ZMQ.SUB);
 //		subscriber.connect("tcp://192.168.1.141:5555");
 //		subscriber.subscribe("toutiao".getBytes());
 
-		subscriber.connect("tcp://192.168.223.43:7779");
-		subscriber.subscribe("proximity".getBytes());
+//		subscriber.connect("tcp://192.168.223.43:7779");
+//		subscriber.subscribe("proximity".getBytes());
 
 		startSubscriber();
 	}
