6216ef2d9d481c51495d97fbe242ae183d68e207
Jose Luis García <jlgc77@gmail.com>
2019-10-17 12:58:47 +0200

    coge todo el json

--- src/main/java/io/wizzie/kafka/connect/zeromq/SampleWithOutKafka.java
+++ src/main/java/io/wizzie/kafka/connect/zeromq/SampleWithOutKafka.java
@@ -6,7 +6,10 @@ import org.zeromq.ZMQ;
 import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.common.primitives.Bytes;
 import com.google.protobuf.ByteString;
 
@@ -22,7 +25,7 @@ public class SampleWithOutKafka {
 		Socket socket = context.socket(ZMQ.SUB);
 		socket.connect("tcp://192.168.223.43:7779");
 		// subscribe to all available topics
-		socket.subscribe("location".getBytes());
+		socket.subscribe("".getBytes());
 //		socket.subscribe("proximity".getBytes());
 //		socket.subscribe("presence".getBytes());
 
@@ -32,30 +35,34 @@ public class SampleWithOutKafka {
 		while (!Thread.currentThread().isInterrupted()) {
 			currentAddress = socket.recvStr(0);
 			currByte = socket.recv(0);
-			System.out.println("CurrentAdress es >>> " + currentAddress);
+//			System.out.println("CurrentAdress es >>> " + currentAddress);
 			LocationData locationData = new LocationData();
 
 			while (socket.hasReceiveMore()) {
-				System.out.println("Entro en while del socker");
+//				System.out.println("Entro en while del socker");
 				byte[] moreBytes = socket.recv(0);
 				currByte = Bytes.concat(currByte, moreBytes);
 			}
 
 			nb_event evento = nb_event.parseFrom(currByte);
-//			System.out.println(evento);
+			System.out.println(evento);
 			if (currentAddress.equalsIgnoreCase("presence")) {
 //				System.out.println("es presence");
 			} else if (currentAddress.equalsIgnoreCase("proximity")) {
 //				System.out.println("es proximity");
-			} else if (currentAddress.equalsIgnoreCase("location")) {
+			} else if (currentAddress.startsWith("location")) {
 				System.out.println("es location");
 				location location = evento.getLocation();
+//				System.out.println(location);
 //				System.out.println("latitude >> " + location.getStaLocationX());
 //				System.out.println("longitude >>" + location.getStaLocationY());
 				locationData.setX(location.getStaLocationX());
 				locationData.setY(location.getStaLocationY());
 				ObjectMapper objectMapper = new ObjectMapper();
-				String json = objectMapper.writeValueAsString(locationData);
+				objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+				objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+				objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+				String json = objectMapper.writeValueAsString(location);
 				System.out.println(json);
 			} else {
 //				System.out.println("es otro");
