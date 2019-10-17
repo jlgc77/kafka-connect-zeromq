2726522e8885e7a0a8716127e5a7bf159d7f0a35
Jose Luis García <jlgc77@gmail.com>
2019-10-17 13:13:05 +0200

    schema

--- src/main/java/io/wizzie/kafka/connect/zeromq/SampleWithOutKafka.java
+++ src/main/java/io/wizzie/kafka/connect/zeromq/SampleWithOutKafka.java
@@ -35,53 +35,22 @@ public class SampleWithOutKafka {
 		while (!Thread.currentThread().isInterrupted()) {
 			currentAddress = socket.recvStr(0);
 			currByte = socket.recv(0);
-//			System.out.println("CurrentAdress es >>> " + currentAddress);
+			System.out.println("CurrentAdress es >>> " + currentAddress);
 			LocationData locationData = new LocationData();
 
 			while (socket.hasReceiveMore()) {
-//				System.out.println("Entro en while del socker");
 				byte[] moreBytes = socket.recv(0);
 				currByte = Bytes.concat(currByte, moreBytes);
 			}
 
 			nb_event evento = nb_event.parseFrom(currByte);
-			System.out.println(evento);
-			if (currentAddress.equalsIgnoreCase("presence")) {
-//				System.out.println("es presence");
-			} else if (currentAddress.equalsIgnoreCase("proximity")) {
-//				System.out.println("es proximity");
-			} else if (currentAddress.startsWith("location")) {
-				System.out.println("es location");
-				location location = evento.getLocation();
-//				System.out.println(location);
-//				System.out.println("latitude >> " + location.getStaLocationX());
-//				System.out.println("longitude >>" + location.getStaLocationY());
-				locationData.setX(location.getStaLocationX());
-				locationData.setY(location.getStaLocationY());
-				ObjectMapper objectMapper = new ObjectMapper();
-				objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
-				objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
-				objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-				String json = objectMapper.writeValueAsString(location);
-				System.out.println(json);
-			} else {
-//				System.out.println("es otro");
-			}
+			ObjectMapper objectMapper = new ObjectMapper();
+			objectMapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
+			objectMapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
+//			objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
+			String json = objectMapper.writeValueAsString(evento);
+			System.out.println(json);
 
-			/**
-			 * Datos proximity
-			 */
-//			proximity item = proximity.parseFrom(currByte);
-//			System.out.println("Entidad >>> ");
-//			System.out.println(item);
-//			System.out.println("Mac address client >>> ");
-//			System.out.println(item.getStaEthMac());
-//			System.out.println(item.getHashedStaEthMac());
-//			String sta_eth_mac = byteStringToStringForMac(item.getHashedStaEthMac());
-//			String sta_eth_mac_utf8 = item.getHashedStaEthMac().toStringUtf8();
-//			System.out.println(sta_eth_mac);
-//			System.out.println(sta_eth_mac_utf8);
-//			System.out.println("Fin mac >>> ");
 		}
 	}
 
